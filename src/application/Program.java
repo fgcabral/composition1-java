9e0c8fbf5c3f78bb4d12de31d4770452f1baf184
Fabio Cabral <fgcabral@gmail.com>
2020-10-17 22:32:06 -0300

    Mensagem teste git

--- src/application/Program.java
+++ src/application/Program.java
@@ -53,7 +53,7 @@ public class Program {
 		System.out.println("Name: " + worker.getName());
 		System.out.println("Department: " + worker.getDepartment().getName());
 		System.out.println("Income for " + monthAndYear + ": " + String.format("%.2f", worker.income(year, month)));
-		
+		System.out.println("teste git");
 		sc.close();
 	}
 }
